5702d43f114538de9f6f302dc696aa6aec77fe97
TramVM <tramvm@gmail.com>
2021-10-20 11:43:38 +0700

    Send socket only when donate success

--- code/common/tasks.py
+++ code/common/tasks.py
@@ -33,24 +33,26 @@ def donate_direct_task(payload):
     print(data)
     result = call_payment_gw_api('/v1/payment/loyalty/donate', data)
 
-    socket_payload = {
-        "type": "public",
-        "room": payload["stream_id"],
-        "author_id": payload["receiver"],
-        "event": "donate",
-        "payload": {
-            "content": "<span style='color: #FAAD14;'>{}</span> đã donate <span style='color: #FAAD14;'>{} RZP</span><br/>{}".format(user_detail["user_full_name"], payload["amount"], payload["note"]),
-            "amount": payload["amount"],
-            "user": {
-                "user_name": user_detail["user_full_name"] or '',
-                "user_avatar": user_detail["user_avatar"] or '',
-            },
-            "message": payload["note"],
-        },
-        "users": []
-    }
+    if result and result['status'] == 1 and not result['error_code']:
 
-    # Send message donating
-    call_socket_api('/v1/socket/send_to_room', socket_payload)
+        socket_payload = {
+            "type": "public",
+            "room": payload["stream_id"],
+            "author_id": payload["receiver"],
+            "event": "donate",
+            "payload": {
+                "content": "<span style='color: #FAAD14;'>{}</span> đã donate <span style='color: #FAAD14;'>{} RZP</span><br/>{}".format(user_detail["user_full_name"], payload["amount"], payload["note"]),
+                "amount": payload["amount"],
+                "user": {
+                    "user_name": user_detail["user_full_name"] or '',
+                    "user_avatar": user_detail["user_avatar"] or '',
+                },
+                "message": payload["note"],
+            },
+            "users": []
+        }
+
+        # Send message donating
+        call_socket_api('/v1/socket/send_to_room', socket_payload)
 
     return result
